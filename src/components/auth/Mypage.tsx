6a1f29c47b02ba0d2b119c7acbee8355bbc25bc5
unknown <naodg@gmail.com>
2025-05-27 16:45:42 +0900

    냥

--- src/components/auth/Mypage.tsx
+++ src/components/auth/Mypage.tsx
@@ -71,12 +71,14 @@ export default function Mypage() {
         <div className="user-view">
           <h3>사용자 마이페이지</h3>
           <ul>
-            <li>내가 쓴 리뷰 보기</li>
+            <li
+              onClick={() => { navigate(`/mypage/myreview`); }}
+            >내가 쓴 리뷰 보기</li>
             <li
               onClick={() => { navigate(`/mypage/mystore`); }}
             >찜한 가게 목록</li>
             <li
-              onClick={() => { navigate(`/mypage/myreview`); }}
+            // onClick={() => { navigate(`/mypage/myreview`); }}
             >내 정보 수정</li>
           </ul>
         </div>
