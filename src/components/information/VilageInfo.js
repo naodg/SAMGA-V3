214f3b8addded163cf06b43e92f3a803f8b98d96
naondg <naondg@gmail.com>
2025-06-16 09:21:22 +0900

    우리마을소개 1차 ㅇㅋㅇㅋ  퐁퐁

--- src/components/information/VilageInfo.js
+++ src/components/information/VilageInfo.js
@@ -1,5 +1,5 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { jsx as _jsx } from "react/jsx-runtime";
 import "./VilageInfo.css";
 export default function VilageInfo() {
-    return (_jsxs("div", { className: "vilage-info-page", children: [_jsx("h1", { className: "vilage-title", children: "\uC6B0\uB9AC \uB9C8\uC744 \uC18C\uAC1C" }), _jsx("div", { className: "vilage-images", children: _jsx("img", { src: "/samga/information/information.jpg" }) })] }));
+    return (_jsx("div", { className: "vilage-info-page", children: _jsx("div", { className: "vilage-images", children: _jsx("img", { src: "/samga/information/information.jpg" }) }) }));
 }
--- src/components/information/VilageInfo.tsx
+++ src/components/information/VilageInfo.tsx
@@ -4,7 +4,7 @@ import "./VilageInfo.css";
 export default function VilageInfo() {
   return (
     <div className="vilage-info-page">
-      <h1 className="vilage-title">우리 마을 소개</h1>
+      {/* <h1 className="vilage-title">우리 마을 소개</h1> */}
       <div className="vilage-images">
           <img src="/samga/information/information.jpg" />
       </div>
