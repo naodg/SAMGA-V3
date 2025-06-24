c676309c5f4233f5282eddb1e6ab5b264f944b04
naondg <naondg@gmail.com>
2025-06-24 12:49:04 +0900

    중간수정png로  퐁퐁

--- src/data/goodsData.js
+++ src/data/goodsData.js
@@ -10,7 +10,7 @@ export const goodsList = [
         id: "keyring",
         name: "소탈이 키링",
         price: 5000,
-        thumbnail: "/samga/goods/keyring_thumb.jpg",
-        images: ["/samga/goods/keyring_detail1.jpg", "/samga/goods/keyring_detail2.jpg", "/samga/goods/keyring_detail3.jpg", "/samga/goods/keyring_detail4.jpg"]
+        thumbnail: "/samga/goods/keyring_thumb.png",
+        images: ["/samga/goods/keyring_detail1.png", "/samga/goods/keyring_detail2.png", "/samga/goods/keyring_detail3.png", "/samga/goods/keyring_detail4.png"]
     },
 ];
