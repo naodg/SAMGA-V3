2852c1f7da82ec7e5543590e7efe8e440ca408ce
naondg <naondg@gmail.com>
2025-06-24 15:03:36 +0900

    상세페이지 이미지까지 보임   퐁퐁

--- src/components/information/GoodsmallDetail.tsx
+++ src/components/information/GoodsmallDetail.tsx
@@ -1,4 +1,3 @@
-// src/pages/GoodsmallDetail.tsx
 import { useParams } from "react-router-dom";
 import { goodsList } from "../../data/goodsData";
 import "./GoodsmallDetail.css";
@@ -11,17 +10,23 @@ export default function GoodsmallDetail() {
 
   return (
     <div className="goods-detail-page">
-      <div className="goods-image-area">
-        <img src={product.images[0]} alt={product.name} />
+      <div className="goods-top-area">
+        <div className="goods-image-area">
+          <img src={product.images[0]} alt={product.name} />
+        </div>
+
+        <div className="goods-info-area">
+          <p className="goods-detail-name">{product.name}</p>
+          <p className="goods-detail-price">{product.price.toLocaleString()}원</p>
+          <p className="goods-detail-delivery">배송방법: 택배 (2,500원)</p>
+          <p className="goods-detail-notice">※ 모든 상품은 품절 상태입니다.</p>
+          <button className="goods-detail-soldout" disabled>SOLD OUT</button>
+        </div>
       </div>
 
-      <div className="goods-info-area">
-        <p className="goods-detail-name">{product.name}</p>
-        <p className="goods-detail-price">{product.price.toLocaleString()}원</p>
-        <p className="goods-detail-delivery">배송방법: 택배 (2,500원)</p>
-        <p className="goods-detail-notice">※ 모든 상품은 품절 상태입니다.</p>
-
-        <button className="goods-detail-soldout" disabled>SOLD OUT</button>
+      <div className="goods-extra-section">
+        {/* <h3>{product.id === "keyring" ? "키링 상세 설명" : "피규어 상세 설명"}</h3> */}
+        <img src={product.detailimage} alt={`${product.name} 상세`} />
       </div>
     </div>
   );
