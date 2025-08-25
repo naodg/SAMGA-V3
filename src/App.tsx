f95ea73260e9c57ed5170e1bec154aa4ef538832
YeahU <elen02@naver.com>
2025-08-25 09:24:26 +0900

    hash router 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import StoreDetail from './components/store/store_detail'
