4f4b735f21fa7c03be17aab4b27e5ed9a0439ff9
YeahU <elen02@naver.com>
2025-12-17 11:39:58 +0900

    삼가 sitemap, rss, router 업데이트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import StoreDetail from './components/store/store_detail';
